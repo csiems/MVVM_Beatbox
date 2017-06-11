f05cb28e0b867515152bd41f51965cbaee00a033
Christopher Siems <csiems@gmail.com>
2017-06-10 17:40:28 -0700

    create load and play methods

---
+++ app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.beatbox;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class SoundViewModelTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}

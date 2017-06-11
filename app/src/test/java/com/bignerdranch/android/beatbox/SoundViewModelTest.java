ca3fd64011e0ee6fc322365c35952103e9fdb73f
Christopher Siems <csiems@gmail.com>
2017-06-10 17:40:49 -0700

    set up testing libraries and class

--- app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.java
+++ app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.java
@@ -2,8 +2,6 @@ package com.bignerdranch.android.beatbox;
 
 import org.junit.Before;
 
-import static org.junit.Assert.*;
-
 public class SoundViewModelTest {
     @Before
     public void setUp() throws Exception {
